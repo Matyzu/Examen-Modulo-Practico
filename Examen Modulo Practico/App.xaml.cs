edb52e1019837031ccf9b0e038425b429626acbb
MatyZu <matiaszulesacosta@gmail.com>
2026-05-17 15:01:55 -0500

    Agregar archivos de proyecto.

---
+++ Examen Modulo Practico/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Examen_Modulo_Practico
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
